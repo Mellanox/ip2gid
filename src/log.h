7f8db9b1c7178cb0bf8c38e3e29397394cd68035
Mark Zhang <markzhang@nvidia.com>
2022-12-17 11:00:58 +0200

    nl/rdma: Set a larger nl_rdma socket SO_RCVBUF
    Set a larger nl_rdma socket SO_RCVBUF to reduce the ENOBUFS error:
    "ERR run_nl_rdma_listen: nl_rdma_recv failed 105, errno 105"
    
    Signed-off-by: Mark Zhang <markzhang@nvidia.com>

--- src/log.h
+++ src/log.h
@@ -37,6 +37,9 @@ enum {
 #define nl_log_info(format, ...) \
 	resolv_write(RESOLV_LOG_INFO, " %s: "format, __func__, ## __VA_ARGS__)
 
+#define nl_log_warn(format, ...) \
+	resolv_write(RESOLV_LOG_WARN, " %s: "format, __func__, ## __VA_ARGS__)
+
 #define nl_log_err(format, ...) \
 	resolv_write(RESOLV_LOG_ERR, " %s: "format, __func__, ## __VA_ARGS__)
 
